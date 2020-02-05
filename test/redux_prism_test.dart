952dee79d7df33e21a16037c65e970fa1912647f
bgildson <gildson13@hotmail.com>
2020-02-04 23:49:23 -0300

    added tests for StorePrismListener widget

--- test/redux_prism_test.dart
+++ test/redux_prism_test.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 import 'package:redux_prism/redux_prism.dart';
 
@@ -22,5 +25,35 @@ void main() {
     expectLater(StorePrism.actions, emitsInOrder([action]));
 
     StorePrism.middleware(store, action, next);
+  });
+
+  testWidgets('should listen dispatched actions in StorePrismListener.listen callback', (WidgetTester tester) async {
+    int actionsCounter = 0;
+
+    final store = Store<String>(
+      (s, a) => s,
+      initialState: 'S',
+      middleware: [StorePrism.middleware],
+    );
+
+    final widget = StoreProvider<String>(
+      store: store,
+      child: StorePrismListener(
+        listen: (a) {
+          actionsCounter++;
+        },
+        child: Container(),
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+
+    expect(actionsCounter, 0);
+
+    store.dispatch('I');
+
+    await tester.pumpWidget(widget);
+
+    expect(actionsCounter, 1);
   });
 }
